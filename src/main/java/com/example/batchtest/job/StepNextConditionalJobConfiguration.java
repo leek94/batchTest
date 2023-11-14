7f15dfcddbf40c579d6e22703addc700b370cffd
leek94 <khwan929@naver.com>
2023-11-14 12:16:39 +0900

    feat: 배치 역할 분담 조건 분할 생성

--- src/main/java/com/example/batchtest/job/StepNextConditionalJobConfiguration.java
+++ src/main/java/com/example/batchtest/job/StepNextConditionalJobConfiguration.java
@@ -2,7 +2,6 @@ package com.example.batchtest.job;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -47,7 +46,7 @@ public class StepNextConditionalJobConfiguration {
                      ExitStatus를 FAILED로 지정한다.
                      해당 status를 보고 flow가 진행된다.
                      **/
-                    contribution.setExitStatus(ExitStatus.FAILED);
+//                    contribution.setExitStatus(ExitStatus.FAILED);
 
                     return RepeatStatus.FINISHED;
                 })
